1e49ece9c3d93d37c814f64f4e206f3e30af2a88
Mirrodiya12 <125704692+Mirrodiya12@users.noreply.github.com>
2023-05-18 23:13:23 -0500

    Commit #2 avance frontend

--- config.php
+++ config.php
@@ -1,18 +1,15 @@
 <?php
 
-// Datos de conexion a la base de datos
 $servidor='localhost';
 $usuario='root';
 $pass='';
-$bd='eventos';
+$bd='doo';
 
-//Conexion a la base de datos
+// Nos conectamos a la base de datos
 $conexion = new mysqli($servidor, $usuario, $pass, $bd);	
 
-// Datos en utf8
 $conexion->set_charset('utf8');
 
-// verificamos si hubo algun error y lo mostramos
 if ($conexion->connect_errno) {
 	echo "Error al conectar la base de datos {$conexion->connect_errno}";
 }
--- index.php
+++ index.php
@@ -1,10 +1,52 @@
 <?php
 
-date_default_timezone_set("America/Santiago");
+date_default_timezone_set("America/Bogota");
 
-// incluimos el archivo de configuracion
 include 'config.php';
 
-?>
+if (isset($_POST['from'])) 
+{
 
+    if ($_POST['from']!="" AND $_POST['to']!="") 
+    {
+
+        $Datein                    = date('d/m/Y H:i:s', strtotime($_POST['from']));
+        $Datefi                    = date('d/m/Y H:i:s', strtotime($_POST['to']));
+        $inicio = _formatear($Datein);
+
+        $final  = _formatear($Datefi);
+
+        $orderDate                      = date('d/m/Y H:i:s', strtotime($_POST['from']));
+        $inicio_normal = $orderDate;
+
+        $orderDate2                      = date('d/m/Y H:i:s', strtotime($_POST['to']));
+        $final_normal  = $orderDate2;
+
+        $titulo = evaluar($_POST['title']);
+
+        $body   = evaluar($_POST['event']);
+
+        $clase  = evaluar($_POST['class']);
+
+        $query="INSERT INTO agenda VALUES(null,'$titulo','$body','','$clase','$inicio','$final','$inicio_normal','$final_normal')";
+
+        $conexion->query($query)or die('<script type="text/javascript">alert("Horario No Disponible ")</script>');
+
+        header("Location:$base_url");        
+
+
+        $im=$conexion->query("SELECT MAX(id) AS id FROM agenda");
+        $row = $im->fetch_row();  
+        $id = trim($row[0]);
+
+        $link = "$base_url"."descripcion_evento.php?id=$id";
+
+        $query="UPDATE agenda SET url = '$link' WHERE id = $id";
+
+        $conexion->query($query); 
+
+    }
+}
+
+?>
 
